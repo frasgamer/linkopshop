dd7a6ff3e2dcc69736480c3760f8bf6cb9263446
frasgamer <44925264+frasgamer@users.noreply.github.com>
2018-12-17 22:34:16 +0300

    Add files via upload

---
+++ link.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    if (message.content.startsWith("OP SHOP")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 9999999999,
+        maxAge: 999999999,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة OP SHOP :heart: :dove:")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : لانهائي| عدد استخدامات الرابط : لانهائي **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+
+client.login(process.env.BOT_TOKEN);
+
